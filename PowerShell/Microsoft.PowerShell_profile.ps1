f7d1cc39180d1c38d5ba42d5e66f2ed6646e4b90
sergeygurev <155543369+sergeygurev@users.noreply.github.com>
2024-01-03 17:37:17 +0300

    Create Microsoft.PowerShell_profile.ps1

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,40 @@
+#set PowerShell to UTF-8
+#System.Text.UTF8Encoding
+Import-Module posh-git
+$omp_config = Join-Path $PSScriptRoot ".\gurevsergey.omp.json"
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+#Get-PoshThemes 1_shell
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Env
+$env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
+
+
+
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+
+
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue}
+
+
